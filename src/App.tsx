ad3e96b8022e6def023e2ef2e988971a1700282a
eliamfuentes <eliamfuentes@ufcspa.edu.br>
2025-07-28 16:00:52 -0300

    Finalizando a tela principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,60 @@
-import { Heading } from "./components/Heading";
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+import { CountDown } from "./components/CountDown";
+import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
+import { DefaultButton } from "./components/DefaultButton";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import { PlayCircleIcon } from "lucide-react";
+import { Footer } from "./components/Footer";
 
 export function App() {
   return (
     <>
-      <Heading>Olá mundo</Heading>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore veniam
-        ducimus hic. Aperiam obcaecati culpa consectetur. Beatae, debitis. Ipsam
-        unde reprehenderit pariatur est sit laborum illum magni accusamus beatae
-        iure.
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form action="" className="form">
+          <div className="formRow">
+            <DefaultInput
+              id="input"
+              type="text"
+              labelText="Tarefa"
+              placeholder="Digite sua tarefa"
+              disabled
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <Cycles />
+          </div>
+
+          <div className="formRow">
+            <DefaultButton icon={<PlayCircleIcon />} />
+          </div>
+        </form>
+      </Container>
+
+      <Container>
+        <Footer />
+      </Container>
     </>
   );
 }
