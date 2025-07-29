ebc568204d3bba1293358bde6012f9177cdee8f4
eliamfuentes <eliamfuentes@ufcspa.edu.br>
2025-07-29 17:07:40 -0300

    Criando pages e templates

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
-import { DefaultInput } from "./components/DefaultInput";
-import { Cycles } from "./components/Cycles";
-import { DefaultButton } from "./components/DefaultButton";
+import { Footer } from "./components/Footer";
+import { MainForm } from "./components/MainForm";
 
 import "./styles/theme.css";
 import "./styles/global.css";
-import { PlayCircleIcon } from "lucide-react";
-import { Footer } from "./components/Footer";
 
 export function App() {
   return (
@@ -27,29 +24,7 @@ export function App() {
       </Container>
 
       <Container>
-        <form action="" className="form">
-          <div className="formRow">
-            <DefaultInput
-              id="input"
-              type="text"
-              labelText="Tarefa"
-              placeholder="Digite sua tarefa"
-              disabled
-            />
-          </div>
-
-          <div className="formRow">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className="formRow">
-            <Cycles />
-          </div>
-
-          <div className="formRow">
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
+        <MainForm />
       </Container>
 
       <Container>
